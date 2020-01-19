c73eb4c86ae35adea254d6e979623ed7f5de8ac3
Mohammad Rajabpour <50981979+rajabpour4097@users.noreply.github.com>
2020-01-19 18:30:32 +0330

    Create SVMinHeartdataset.py

---
+++ SVMinHeartdataset.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+
+print(__doc__)
+
+data = pd.read_csv('heart.csv')
+X = data.iloc[:, :-1].values
+y = data.iloc[:, 13].values
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.5, random_state=0)
+
+tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-1,1e-2,1e-3, 1e-4,1e-5,1e-6,1e-7],
+                     'C': range(1, 100000, 10)},
+                    {'kernel': ['linear'], 'C': range(1, 100000, 10)}]
+
+scores = ['precision', 'recall']
+
+for score in scores:
+    print("# Score: %s" % score)
+    print()
+
+    clf = GridSearchCV(SVC(), tuned_parameters, cv=5,
+                       scoring='%s_macro' % score)
+    clf.fit(X_train, y_train)
+
+    print()
+    print(clf.best_params_)
+    print()
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+    print()
+
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    print()
